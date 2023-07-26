f312368c0531f9c65556b112fab890b19f2fe67d
inhwamo <inhwa2000@gmail.com>
2023-07-25 18:18:32 -0700

    refactored scorekeeper, started async js section

--- ScoreKeeper/ScoreKeeperPersonalAttempt/app.js
+++ ScoreKeeper/ScoreKeeperPersonalAttempt/app.js
@@ -3,45 +3,32 @@ const p1Button = document.querySelector('#p1');
 const p2Button = document.querySelector('#p2');
 const resetButton = document.querySelector('#reset');
 const winningScoreElement = document.querySelector('#playto');
-let winningScore  = Number(winningScoreElement.value);
 const p1ScoreDisplay = document.querySelector('#p1ScoreDisplay');
 const p2ScoreDisplay = document.querySelector('#p2ScoreDisplay');
 
-
 let p1Score = 0;
 let p2Score = 0;
-//let isGameOver = false; 
+let winningScore = Number(winningScoreElement.value);
 
-p1Button.addEventListener('click', function () {
-    if (p1Score < winningScore) { //if (!isGameOver)
-        p1Score += 1;
-        p1ScoreDisplay.textContent = p1Score;
-        if (p1Score === winningScore) {
-            p1ScoreDisplay.classList.add('has-text-success');
-            p2ScoreDisplay.classList.add('has-text-danger');
-            p1Button.disabled = true;
-            p2Button.disabled = true;
+function updateScore(playerScore, playerScoreDisplay, opponentScoreDisplay) {
+    if (playerScore < winningScore) {
+        playerScore += 1;
+        playerScoreDisplay.textContent = playerScore;
+        if (playerScore === winningScore) {
+            endGame(playerScoreDisplay, opponentScoreDisplay);
         }
     }
-})
+    return playerScore;
+}
 
-p2Button.addEventListener('click', function () {
-    if (p2Score < winningScore) {
-        p2Score += 1;
-        p2ScoreDisplay.textContent = p2Score;
-        if (p2Score === winningScore) {
-            p1ScoreDisplay.classList.add('has-text-danger');
-            p2ScoreDisplay.classList.add('has-text-success');
-            p1Button.disabled = true;
-            p2Button.disabled = true;
-        }
-    }
-})
-winningScoreElement.addEventListener('change', function () {
-    winningScore = Number(winningScoreElement.value); //Number() stricter than ParseInt()
+function endGame(winningScoreDisplay, losingScoreDisplay) {
+    winningScoreDisplay.classList.add('has-text-success');
+    losingScoreDisplay.classList.add('has-text-danger');
+    p1Button.disabled = true;
+    p2Button.disabled = true;
+}
 
-})
-resetButton.addEventListener('click', function () {
+function resetGame() {
     p1Score = 0;
     p2Score = 0;
     p1ScoreDisplay.textContent = p1Score;
@@ -50,8 +37,18 @@ resetButton.addEventListener('click', function () {
     p2ScoreDisplay.classList.remove('has-text-success', 'has-text-danger');
     p1Button.disabled = false;
     p2Button.disabled = false;
-})
+}
 
-// function reset(){
+p1Button.addEventListener('click', function () {
+    p1Score = updateScore(p1Score, p1ScoreDisplay, p2ScoreDisplay);
+});
 
-// }
+p2Button.addEventListener('click', function () {
+    p2Score = updateScore(p2Score, p2ScoreDisplay, p1ScoreDisplay);
+});
+
+winningScoreElement.addEventListener('change', function () {
+    winningScore = Number(winningScoreElement.value);
+});
+
+resetButton.addEventListener('click', resetGame);
